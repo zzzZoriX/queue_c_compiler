0e6513ef24745365430969f39a203c0b011f65d0
vstlvl <zorix228337@gmail.com>
2025-03-24 21:16:04 +0300

    переписал фукнцию парса вызывающей комманды

--- libs/dot_c/_cmd.c
+++ libs/dot_c/_cmd.c
@@ -7,15 +7,28 @@ parse(int argc, int* start, int* end, char** args){
         return _TOO_FEW_ARGS;
     else if(strcmp(args[1], "-otp") == 0)
         return _NO_INP_FILE;
-    else if(strcmp(args[argc - 1], "-otp") == 0)
-        return _NO_OTP_FILE;
 
-    *start = 1; // второй аргумент
-    int file = 1;
-    while(strcmp(args[file], "-otp") != 0)
-        ++file;
+    *start = 1;
 
-    *end = file;
+    int otp_flag_pos = -1;
+    for(int i = 1; i < argc; ++i){
+        if(!strcmp(args[i], "-otp")){
+            if(i == argc - 1) return _NO_OTP_FILE;
+            if(otp_flag_pos != -1) return _MULTI_OTP_FLAG;
 
-    return _SUCCESS;
+            otp_flag_pos = i;
+        }
+    }
+
+    *end = otp_flag_pos == -1 ? argc : otp_flag_pos;
+
+    for(int i = *start; i < *end; ++i){
+#ifdef _WIN32
+    if(!file_exists(args[i])) return _INPUT_FILE_NOT_FOUND; 
+#else
+    if(access(args[i], F_OK) != 0) return _INPUT_FILE_NOT_FOUND;
+#endif
+    }
+
+    return otp_flag_pos == -1 ? _SUCCESS_BUT_WO_OTP : _SUCCESS;
 }
