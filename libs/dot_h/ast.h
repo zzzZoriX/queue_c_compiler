10cb18ec6a8d70fd589e8efa16bae7c964fba7b8
vstlvl <zorix228337@gmail.com>
2025-04-03 21:53:18 +0300

    добавил определения фукнций создания объекта узла

--- libs/dot_h/ast.h
+++ libs/dot_h/ast.h
@@ -1,6 +1,7 @@
 #ifndef AST_H
 #define AST_H
 
+#include "lexeme.h"
 #include "str.h"
 
 typedef enum _node_type {
@@ -29,10 +30,22 @@ typedef struct AST_Node {
     _node_type node_type;
 
     union {
-        string obj_name;
+        string obj_name, type;
+        union {
+            int int_value;
+            char char_value;
+            float flt_value;
+            bool bool_value;
+            struct AST_Node* node_value;
+        } value; // обычный объект
 
-        double num_value;
-        struct AST_Node* node_val;
+        union {
+            int* p_int_value;
+            char* p_char_value;
+            float* p_float_value;
+            bool* p_bool_value;
+            struct AST_Node** p_node_value;
+        } p_value; // указатель
 
         struct {
             _op_type type;
@@ -47,4 +60,84 @@ typedef struct AST_Node {
     } data;
 } Node;
 
+
+/**
+ * @brief создает узел интового объекта
+ * 
+ * @return Node* 
+ */
+Node*
+create_int_obj_node(const string, const int);
+
+/**
+ * @brief создает узел символьного объекта
+ * 
+ * @return Node* 
+ */
+Node*
+create_char_obj_node(const string, const char);
+
+/**
+ * @brief создает узел флоат объекта
+ * 
+ * @return Node* 
+ */
+Node*
+create_flt_obj_node(const string, const float);
+
+/**
+ * @brief создает узел булевого объекта
+ * 
+ * @return Node* 
+ */
+Node*
+create_bool_obj_node(const string, const bool);
+
+/**
+ * @brief создает узел объекта
+ * 
+ * @return Node* 
+ */
+Node*
+create_obj_node(const string, const Node*);
+
+/**
+ * @brief создает узел указателя интового объекта
+ * 
+ * @return Node* 
+ */
+ Node*
+ create_p_int_obj_node(const string, const int*);
+ 
+ /**
+  * @brief создает узел указателя символьного объекта
+  * 
+  * @return Node* 
+  */
+ Node*
+ create_p_char_obj_node(const string, const char*);
+ 
+ /**
+  * @brief создает узел указателя флоат объекта
+  * 
+  * @return Node* 
+  */
+ Node*
+ create_p_flt_obj_node(const string, const float*);
+ 
+ /**
+  * @brief создает узел указателя булевого объекта
+  * 
+  * @return Node* 
+  */
+ Node*
+ create_p_bool_obj_node(const string, const bool*);
+ 
+ /**
+  * @brief создает узел указателя объекта
+  * 
+  * @return Node* 
+  */
+ Node*
+ create_p_obj_node(const string, const Node**);
 #endif
