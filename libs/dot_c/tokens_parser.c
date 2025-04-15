be0d1cf1dc174f36618a0dbcbe84ae3d34085f4e
vstlvl <zorix228337@gmail.com>
2025-04-15 16:22:11 +0300

    написал кейсы для long и short

--- libs/dot_c/tokens_parser.c
+++ libs/dot_c/tokens_parser.c
@@ -19,24 +19,33 @@ tokens_parser(_token* tokens){
                         obj_node = make_flt_literal_const(
                             current_token->data, 
                             atof(current_token->next_token->next_token->data)
-                        );
+                        ); break;
                     case LEX_INT:
                         obj_node = make_int_literal_const(
                             current_token->data,
                             atoi(current_token->next_token->next_token->data)
-                        );
+                        ); break;
                     case LEX_CHAR:
-                        obj_node = make_char_literaL_const(
+                        obj_node = make_char_literal_const(
                             current_token->data,
                             current_token->next_token->next_token->data[1]
-                        );
+                        ); break;
                     case LEX_BOOL:
                         obj_node = make_bool_literal_const(
                             current_token->data,
                             (comp(current_token->next_token->next_token->data, "true") ? true : false)
-                        );
-
-                    // long и short реализую позже
+                        ); break;
+                    case LEX_LONG:
+                        obj_node = make_long_literal_const(
+                            current_token->data,
+                            atol(current_token->next_token->next_token->data)
+                        ); break;
+                    case LEX_SHORT:
+                        obj_node = make_short_literal_const(
+                            current_token->data,
+                            atosh(current_token->next_token->next_token->data)
+                        ); break;
+                    default: break;
                 }
                 break;
         }
