2984abe2d9e9eb647794d56c50aeb172a369f929
vstlvl <zorix228337@gmail.com>
2025-05-09 12:36:59 +0300

    написал первую готовую версию парсера
    в дальнейшем будут фикситься возможные баги и добавляться новые кейсы

--- libs/dot_c/tokens_parser.c
+++ libs/dot_c/tokens_parser.c
@@ -1,9 +1,25 @@
 #include "c:/queue_c_compiler/libs/dot_h/tokens_parser.h"
-#include <string.h>
+
+
+static inline bool __fastcall
+is_assign(_lexemes lex){
+    return 
+        lex == LEX_ASSIGN       ||
+        lex == LEX_ADD_ASSIGN   ||
+        lex == LEX_REDUCE_ASSIGN||
+        lex == LEX_DIV_ASSIGN   ||
+        lex == LEX_MUL_ASSIGN   ||
+        lex == LEX_REM_ASSIGN
+    ;
+}
+
 
 Node*
 tokens_parser(_token** token){
     switch ((*token)->lex){
+
+/* ------------ литеральные константы ------------ */
+
         case LEX_INT:
         case LEX_LONG:
         case LEX_SHORT:
@@ -23,15 +39,11 @@ tokens_parser(_token** token){
             
             *token = NEXT_TOKEN(*token);
 
-            char bin_op = (*token)->data[0];
+            return lit_const;
 
-            *token = NEXT_TOKEN(*token);
+/* ------------ литеральные константы ------------ */
 
-            return make_bin_operation(
-                lit_const, 
-                tokens_parser(token), 
-                bin_op
-            );
+/* ------------ выражения ------------ */
 
         case LEX_LPAREN:
         case LEX_DIGIT:
@@ -66,6 +78,231 @@ tokens_parser(_token** token){
                 operand,
                 op
             );
+
+/* ------------ выражения ------------ */
+
+/* ------------ if-else ------------ */
+
+        case LEX_IF:
+        case LEX_ELIF:
+        case LEX_ELSE:
+            _lexemes if_else_lexeme = (*token)->lex;
+            Node
+                * condition,
+                * _else
+            ;
+
+            if((*token)->lex != LEX_ELSE){
+                *token = NEXT_TOKEN(*token);
+
+                 condition = make_cond_node(token); // парсим условие
+            }
+            *token = NEXT_TOKEN(*token);
+                
+            Node* body = tokens_parser(token); // парсим тело
+            *token = NEXT_TOKEN(*token);
+
+            if((*token)->lex == LEX_ELIF || (*token)->lex == LEX_ELSE)
+                _else = tokens_parser(token);
+
+            return make_if_else_node(
+                if_else_lexeme,
+                condition,
+                body,
+                _else
+            );
+
+/* ------------ if-else ------------ */
+
+/* ------------ ввод/вывод ------------ */
+
+        case LEX_IN:
+        case LEX_OUT:
+            size_t curr_args_count = 0;
+            _lexemes io_lexeme = (*token)->lex;
+            string format;
+
+            *token = NEXT_TOKEN(NEXT_TOKEN(*token)); // скипаю сразу 2 токена
+
+            format = _strdup((*token)->data);
+            if(!format) exit(1);
+
+            *token = NEXT_TOKEN(*token);
+            if((*token)->lex == LEX_LPAREN)
+                return make_io_node(
+                    io_lexeme,
+                    format,
+                    NULL,
+                    curr_args_count
+                );
+            *token = NEXT_TOKEN(*token);
+
+            ++curr_args_count;
+            string* args = (string*)malloc(sizeof(string) * curr_args_count);
+            if(!args) exit(1);
+
+            args[curr_args_count - 1] = _strdup((*token)->data);
+            if(!args[curr_args_count - 1]) exit(1);
+            *token = NEXT_TOKEN(*token);
+
+            while(*token && (*token)->lex != LEX_LPAREN){
+                ++curr_args_count;
+                
+                args = (string*)realloc(args, sizeof(string) * curr_args_count);
+                if(!args) exit(1);
+
+                args[curr_args_count - 1] = _strdup((*token)->data);
+                if(!args[curr_args_count - 1]) exit(1);
+                *token = NEXT_TOKEN(*token);
+            }
+            
+            return make_io_node(
+                io_lexeme,
+                format,
+                args,
+                curr_args_count
+            );
+
+/* ------------ ввод/вывод ------------ */
+
+/* ------------ цикл for ------------ */
+
+        case LEX_FOR:
+            Node
+                * for_cond,
+                * for_body,
+                * iter
+            ;
+            
+            *token = NEXT_TOKEN(*token);
+
+            for_cond = make_cond_node(token);
+            *token = NEXT_TOKEN(*token);
+
+            if((*token)->lex == LEX_OBJ_NAME){
+                string var_name = _strdup((*token)->data);
+                if(!var_name) exit(1);
+
+                *token = NEXT_TOKEN(*token);
+
+                if(is_assign((*token)->lex)){
+                    string assign = _strdup((*token)->data);
+                    if(!assign) exit(1);
+
+                    *token = NEXT_TOKEN(*token);
+
+                    Node* expr = make_expr_node(token);
+
+                    iter = make_bin_operation(
+                        make_empty_literal_const(var_name),
+                        expr,
+                        assign
+                    );
+                }
+                else if((*token)->lex == LEX_POST_INC || (*token)->lex == LEX_POST_DEC){
+                    string op = _strdup((*token)->data);
+                    if(!op) exit(1);
+
+                    *token = NEXT_TOKEN(*token);
+
+                    iter = make_un_operation(
+                        make_empty_literal_const(var_name),
+                        op
+                    );
+                }
+    
+                else    iter = make_empty_literal_const(var_name);
+
+                free(var_name);
+            }
+            else if((*token)->lex == LEX_PREF_DEC || (*token)->lex == LEX_PREF_INC){
+                string op = _strdup((*token)->data);
+                if(!op) exit(1);
+                *token = NEXT_TOKEN(*token);
+
+                Node* operand = make_expr_node(token);
+                *token = NEXT_TOKEN(*token);
+
+                iter = make_un_operation(
+                    operand,
+                    op
+                );
+            }
+            else{
+                iter = make_expr_node(token);
+                *token = NEXT_TOKEN(*token);
+            }
+
+            for_body = tokens_parser(token);
+
+            return make_for_cycle_node(
+                for_cond,
+                for_body,
+                iter
+            );
+
+/* ------------ цикл for ------------ */
+
+/* ------------ цикл while ------------ */
+
+// LEX_WHILE не рассматривается, потому что синтаксис обязывает писать сначала do, а потом while
+            case LEX_DO:
+                Node
+                    * while_cond,
+                    * while_body
+                ;
+                _lexemes lexeme = (*token)->lex;
+
+                *token = NEXT_TOKEN(NEXT_TOKEN(*token));
+
+                while_cond = make_cond_node(token);
+                while_body = tokens_parser(token);
+
+                return make_do_while_node(
+                    lexeme,
+                    while_cond,
+                    while_body
+                );
+
+/* ------------ цикл while ------------ */
+
+/* ------------ стейтменты ------------ */
+
+// ->
+            case LEX_INST_POINTER:
+                Node* single_command = (Node*)malloc(sizeof(Node));
+                if(!single_command) exit(1);
+
+                single_command->node_type = AST_SINGLE_STMT;
+                *token = NEXT_TOKEN(*token);
+
+                single_command->op1 = tokens_parser(token);
+
+                return single_command;
+
+// {};
+            case LEX_LFPAREN:
+                Node* statements = (Node*)malloc(sizeof(Node));
+                if(!statements) exit(1);
+
+                statements->node_type = AST_MULTI_STMT;
+                while((*token = NEXT_TOKEN(*token))->lex != LEX_RFPAREN){
+                    Node* command = tokens_parser(token);
+
+                    statements->op1 = command;
+
+                    Node* next = (Node*)malloc(sizeof(Node));
+                    if(!statements) exit(1);
+
+                    next = tokens_parser(token);
+
+                    statements->op2 = next;
+                }
+                *token = NEXT_TOKEN(NEXT_TOKEN(*token)); // скипаем };
+
+                return statements;
+
+/* ------------ стейтменты ------------ */
 
         default: break;
     }
